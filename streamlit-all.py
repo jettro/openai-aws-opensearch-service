a79381686e1004da1bdbf8dbffa857f18aed9881
Jettro Coenradie <jettro@coenradie.com>
2023-09-19 21:47:03 +0200

    Big improvement to the functiona calling part. Without using an agent it becomes a lot faster

--- streamlit-all.py
+++ streamlit-all.py
@@ -1,16 +1,14 @@
-import json
 import os
 
 import langchain
 import streamlit as st
-from langchain.vectorstores import OpenSearchVectorSearch
-from langchain.embeddings import OpenAIEmbeddings
-from opensearchpy import RequestsHttpConnection
 from dotenv import load_dotenv
-from langchain.schema import SystemMessage
-from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
-from langchain.tools import Tool
+from langchain.chains.openai_functions import create_openai_fn_chain
 from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate
+from langchain.vectorstores import OpenSearchVectorSearch
+from opensearchpy import RequestsHttpConnection
 
 from retriever import find_auth_opensearch, OpenSearchClient
 
@@ -66,8 +64,14 @@ def init_content_search():
     )
 
 
-def execute_product_search(query: str):
-    found_docs = vector_store_products.similarity_search_with_score(query=query, text_field="title",
+def execute_product_search(query: str) -> dict:
+    """Useful for finding products related to the search terms that are provided.
+
+    Args:
+        query: Query to search products for
+    """
+    found_docs = vector_store_products.similarity_search_with_score(query=query,
+                                                                    text_field="title",
                                                                     vector_field="title_vector")
     results = []
     for doc, _score in found_docs:
@@ -79,14 +83,24 @@ def execute_product_search(query: str):
     }
 
 
-def execute_content_search(query: str):
+def execute_content_search(query: str) -> dict:
+    """Useful for obtaining answers to questions about help for the website, your account and sustainability.
+
+    Args:
+        query: Query to use as input for the content search
+    """
     return {
         "tool": "content_search",
         "result": content_chain.run(query)
     }
 
 
-def execute_opening_hours(city: str):
+def execute_opening_hours(city: str) -> dict:
+    """Useful for obtaining opening hours of a store in city that is provided.
+
+    Args:
+        city: Name of the city to search for
+    """
     body = {
         "query": {
             "match": {
@@ -95,40 +109,32 @@ def execute_opening_hours(city: str):
         }
     }
     search_result = os_client.search(body=body, size=1, index_name="sg-stores")
-    result = []
+    results = []
     if search_result["hits"]["total"]["value"] > 0:
-        result = search_result["hits"]["hits"][0]["_source"]
+        results = search_result["hits"]["hits"][0]["_source"]
 
     return {
         "tool": "opening_hours",
-        "result": result
+        "result": results
     }
 
 
-def init_agent() -> AgentExecutor:
+def init_fn_chain():
     llm = ChatOpenAI(model="gpt-4-0613", temperature=0, openai_api_key=os.getenv('OPEN_AI_API_KEY'))
-    system_message = SystemMessage(
-        content="You are a web service returning the pure json without a change.")
-    prompt = OpenAIFunctionsAgent.create_prompt(system_message=system_message)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a system to search for answers on a provided question."),
+            ("human", "Make calls to the relevant function to using the following input: {input}"),
+            ("human", "Tip: Make sure to answer in the correct format"),
+        ]
+    )
 
-    tools = [
-        Tool(name="ExecuteContentSearch",
-             func=execute_content_search,
-             description="Useful for obtaining answers to questions about help for the website, your account and "
-                         "sustainability."
-             ),
-        Tool(name="ExecuteProductSearch",
-             func=execute_product_search,
-             description="Useful for finding products related to the search terms that are provided."
-             ),
-        Tool(name="ExecuteOpeningHours",
-             func=execute_opening_hours,
-             description="Useful for obtaining opening hours of a store in city that is provided."
-             ),
-    ]
-
-    agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
-    return AgentExecutor(agent=agent, tools=tools, verbose=False)
+    return create_openai_fn_chain(
+        functions=[execute_opening_hours, execute_content_search, execute_product_search],
+        llm=llm,
+        prompt=prompt,
+        verbose=True
+    )
 
 
 if __name__ == '__main__':
@@ -139,7 +145,6 @@ if __name__ == '__main__':
     os_client, config = init_opensearch_client()
     vector_store_products = init_product_search()
     content_chain = init_content_search()
-    agent_executor = init_agent()
 
     # Start Streamlit app
     st.set_page_config(layout="wide")
@@ -149,21 +154,36 @@ if __name__ == '__main__':
 
     the_query = st.text_input('What are you looking for?', key="search_terms_input")
 
-    if the_query:
-        agent_response = agent_executor.run(the_query)
-        response = json.loads(agent_response)
-        if response and response["tool"] == "product_search":
-            for item in response["result"]:
-                st.image(image="./images/" + item['image_name'],
-                         caption=item['title'],
-                         width=200)
-                st.write(item['score'])
+    functions_map = {
+        'execute_opening_hours': execute_opening_hours,
+        'execute_content_search': execute_content_search,
+        'execute_product_search': execute_product_search
+    }
 
-        if response and response["tool"] == "content_search":
-            st.write(response["result"])
-        if response and response["tool"] == "opening_hours":
-            result = response['result']
-            st.subheader(f"Store: {result['store_name']} te {result['city']}")
-            st.write(f"{result['street']} - {result['telephone']}")
-            for ot in result["opening_hours"]:
-                st.write(f"{ot['week_day']} {ot['open_time']} - {ot['closing_time']}")
+    if the_query:
+        the_chain = init_fn_chain()
+        chain_response = the_chain.run(the_query)
+        print(chain_response)
+        function_name = chain_response["name"]
+        args = chain_response["arguments"]
+
+        if function_name not in functions_map:
+            st.write("Could not decide which content search to perform.")
+        else:
+            response = functions_map[function_name](**args)
+
+            if response and response["tool"] == "product_search":
+                for item in response["result"]:
+                    st.image(image="./images/" + item['image_name'],
+                             caption=item['title'],
+                             width=200)
+                    st.write(item['score'])
+
+            if response and response["tool"] == "content_search":
+                st.write(response["result"])
+            if response and response["tool"] == "opening_hours":
+                result = response['result']
+                st.subheader(f"Store: {result['store_name']} te {result['city']}")
+                st.write(f"{result['street']} - {result['telephone']}")
+                for ot in result["opening_hours"]:
+                    st.write(f"{ot['week_day']} {ot['open_time']} - {ot['closing_time']}")
